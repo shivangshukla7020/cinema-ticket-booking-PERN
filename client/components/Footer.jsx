5ca12474a7a9ad2b99d9df321b393f89b3a937c4
shivangshukla7020 <shivangshukla201@gmail.com>
2025-09-20 11:24:38 +0530

    [Shivang] : Added Footer.jsx

---
+++ client/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-4 py-3">
+      <div className="container text-center me-5">
+        <img
+          src="https://www.cgv.vn/skin/frontend/cgv/default/images/bg-cgv/brand-type-film-footer-min-600.png"
+          alt="cgvfooter"
+        />
+      </div>
+      <div className="footer-cgv-address text-center mt-3">
+        <div className="cgv-address-content">
+          <div className="text-cgv-address">
+            <h5>CÔNG TY TNHH CJ CGV VIETNAM</h5>
+            <p className="mb-0">
+              Địa Chỉ:&nbsp;Tầng 2, Rivera Park Saigon - Số 7/28 Thành Thái, P.14, Q.10, TPHCM.
+            </p>
+            <p className="mb-0">Hotline: 1900 6017</p>
+            <p className="mb-0">COPYRIGHT 2021 CJ CGV. All RIGHTS RESERVED.</p>
+            <div className="d-flex justify-content-center mt-2">
+              <div className="logo-footer-cgv"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
